2dc7e954c9921ef19717c21563cb8011a563062b
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-12 15:36:09 -0500

    Exercise 13
    Orbital Strike incoming

---
+++ PlayersGuideExercises/Exercise 13/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Enter X value:");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter Y value:");
+int y = Convert.ToInt32(Console.ReadLine());
+string output;
+
+if (x < 0)
+{
+    output = "W";
+}
+else if (x > 0)
+{
+    output = "E";
+}
+else
+{
+    output = "";
+}
+
+if (y < 0)
+{
+    output = "S" + output;
+}
+else if (y > 0)
+{
+    output = "N" + output;
+}
+else
+{
+    output = "" + output;
+}
+
+Console.WriteLine("Enemy Position:" + output);

d48aeb44bd8f54aaf7fef4be22aecc0d85656d2e
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-12 14:39:03 -0500

    Exercise 9
    Bear Moment

---
+++ PlayersGuideExercises/exercise 9/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("How many eggs were gathered:");
+int eggNumber = Convert.ToInt32(Console.ReadLine());
+int toEach = eggNumber/4;
+int toBear = eggNumber%4;
+Console.WriteLine("To each sister: " + toEach);
+Console.WriteLine("To the Bear: " + toBear);

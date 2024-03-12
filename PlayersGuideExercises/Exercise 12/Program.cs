e29073201a3a7a37d2cb673970ed0b9a28282083
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-12 15:26:23 -0500

    Exercise 12
    Clockwork

---
+++ PlayersGuideExercises/Exercise 12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter number:");
+int input = Convert.ToInt32(Console.ReadLine());
+if (input % 2 == 0)
+{
+    Console.WriteLine("Tick");
+}
+else
+{
+    Console.WriteLine("Tock");
+}

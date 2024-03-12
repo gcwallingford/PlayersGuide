357a3da5ccce7a39e00bd0fe85522ac2eac68b1e
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-12 15:03:28 -0500

    Exercise 11
    math moment

---
+++ PlayersGuideExercises/Exercise 11/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Target Row:");
+int targetRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Target Column:");
+int targetColumn = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Deploy to:");
+Console.WriteLine("(" + --targetRow + ", " + targetColumn + ")");
+Console.WriteLine("(" + targetRow + ", " + --targetColumn + ")");
+Console.WriteLine("(" + ++targetRow + ", " + targetColumn + ")");
+Console.WriteLine("(" + targetRow + ", " + ++targetColumn + ")");

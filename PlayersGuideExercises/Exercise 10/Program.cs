5e6839d1dc4dc7ad9eb4fd9523bfcb29e539e819
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-12 14:44:29 -0500

    Exercise 10
    measuring contest

---
+++ PlayersGuideExercises/Exercise 10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Enter the number of Estates:");
+int estatePoints = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of Duchies:");
+int duchyPoints = 3 * Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter the number of Provinces:");
+int provincePoints = 6 * Convert.ToInt32(Console.ReadLine());
+int total = estatePoints + duchyPoints + provincePoints;
+Console.WriteLine("Total number of points is " + total);

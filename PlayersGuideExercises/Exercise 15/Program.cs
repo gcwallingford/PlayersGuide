1c5dfa4904b17a5a626fa9220caaccc372f7a5c2
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-13 14:58:31 -0500

    Exercise 15
    The missile knows where it is, because it knows where it isn't

---
+++ PlayersGuideExercises/Exercise 15/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("User 1 enter value:");
+int input1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("User 2 enter value:");
+int input2 = Convert.ToInt32(Console.ReadLine());
+input2 /= input1;
+Console.WriteLine(input2);
+do
+{
+    if (input2 == input1)
+    {
+        Console.WriteLine("Entered value is correct. Good Job!");
+        break;
+    }
+
+    if (input2 < input1)
+    {
+        Console.WriteLine("Entered value is too low, try again");
+        input2 = Convert.ToInt32(Console.ReadLine());
+    }
+
+    if (input2 > input1)
+    {
+        Console.WriteLine("Entered value is too high, try again.");
+        input2 = Convert.ToInt32(Console.ReadLine());
+    }
+} while (input2 != input1);

4ae021b3609a1e331d424e8113a96190aee18bce
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-03-13 15:29:32 -0500

    Exercise 16
    she buzz on me til i fizz

---
+++ PlayersGuideExercises/Exercise 16/Program.cs
@@ -0,0 +1,19 @@
+﻿for (int i = 1; i < 100; i++)
+{
+    if (i % 3 == 0 && i % 5 == 0)
+    { 
+        Console.WriteLine("Bofa");
+    }
+    else if (i % 3 == 0)
+    {
+        Console.WriteLine("Fire");
+    }
+    else if (i % 5 == 0)
+    {
+        Console.WriteLine("Lightning");
+    }
+    else
+    {
+        Console.WriteLine("Normal");
+    }
+}

885385da15db93952686a8a83768373d66aa8acc
Guy Wallingford <153114604+gcwallingford@users.noreply.github.com>
2024-04-23 16:21:34 -0500

    31
    bug in logic, have yet to find where

---
+++ PlayersGuideExercises/Exercise 31/Coordinate.cs
@@ -0,0 +1,22 @@
+﻿namespace Exercise_31;
+
+public struct Coordinate(int column = 0 , int row = 0)
+{
+    private int _row = row;
+    private int _column = column;
+
+    public bool isAdjacent(Coordinate coordinate1, Coordinate coordinate2)
+    {
+        bool coordinateAdjacent;
+        if (coordinate1._column == coordinate2._column + 1 || coordinate1._column == coordinate2._column - 1 || coordinate1._row == coordinate2._row + 1 || coordinate1._row == coordinate2._row - 1)
+        {
+            coordinateAdjacent = true;
+        }
+        else
+        {
+            coordinateAdjacent = false;
+        }
+
+        return coordinateAdjacent;
+    }
+}
--- PlayersGuideExercises/Exercise 31/Program.cs
+++ PlayersGuideExercises/Exercise 31/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using Exercise_31;
+
+Coordinate coordinate0 = new Coordinate();
+Coordinate coordinate1 = new Coordinate(1,1);
+Coordinate coordinate2 = new Coordinate(1, 0);
+
+Console.WriteLine(coordinate0.isAdjacent(coordinate0,coordinate1));
+Console.WriteLine(coordinate0.isAdjacent(coordinate1,coordinate2));
+Console.WriteLine(coordinate0.isAdjacent(coordinate0,coordinate2));
